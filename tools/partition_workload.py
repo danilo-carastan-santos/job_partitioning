3af4ca4ed67d62ceaace52cf1f510b3ea843a6f4
CARASTAN DOS SANTOS Danilo <danilo.carastan-dos-santos@inria.fr>
2021-07-08 12:30:58 -0300

    finished partition_workload.py

--- tools/partition_workload.py
+++ tools/partition_workload.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import argparse
 import re
+import subprocess
 
 SEC_ONE_MINUTE=60
 SEC_ONE_HOUR=3600.0
@@ -10,6 +11,8 @@ SEC_ONE_DAY=SEC_ONE_HOUR*24
 SEC_ONE_WEEK=SEC_ONE_DAY*7
 SEC_TWO_WEEKS=SEC_ONE_WEEK*2
 SEC_TWO_DAYS=SEC_ONE_DAY*2
+
+JSON_COMP_SPEED='1e6'
 
 SWF_FIELDS=['JOB_ID',
                 'SUBMIT_TIME',
@@ -66,8 +69,6 @@ def read_input_swf(swf_filename, dct_workload_props):
     SEC_WALLTIME_LIMIT=dct_workload_props[trace_name].max_walltime
     SEC_HALF_WALLTIME_LIMIT=SEC_WALLTIME_LIMIT/2
 
-    
-
     df_workload=pd.read_csv(swf_filename, sep=' ', names=SWF_FIELDS)
 
     lst_workload_newids=[]
@@ -77,16 +78,29 @@ def read_input_swf(swf_filename, dct_workload_props):
     ## assign the new job ids
     df_workload.loc[:,'JOB_ID']=lst_workload_newids
 
+    swf_outfile_name='../workloads/swf/original/original_renamed_jobs/two_weeks/'+trace_name+'/'+basename
     ##save the original workload with the job ids renamed
-    df_workload.to_csv('../workloads/scripttest_original_mustang_release_v0.2.0_66.swf', 
-                                   sep=' ', header=False, index=False)
+    df_workload.to_csv(swf_outfile_name, sep=' ', header=False, index=False)
+
+    #call swftojson and save the json file
+    json_outfile_name='../workloads/json/original/two_weeks/'+trace_name+'/'+re.sub('.swf', '', basename)+'.json'
+    lst_call_command=['python',
+                      'swf_to_batsim_workload_compute_only.py',
+                      '-q',
+                      '-cs', JSON_COMP_SPEED,
+                      '-pf',
+                      str(dct_workload_props[trace_name].max_procs),
+                      swf_outfile_name,
+                      json_outfile_name]
+    #print(lst_call_command)
+    subprocess.call(lst_call_command)
 
     ##long jobs = jobs that will be partitioned
     ##short jobs = jobs that will be unchanged
     df_long_jobs=df_workload.loc[df_workload['REQUESTED_TIME'] >= SEC_HALF_WALLTIME_LIMIT]
     df_short_jobs=df_workload.loc[df_workload['REQUESTED_TIME'] < SEC_HALF_WALLTIME_LIMIT]
     
-    return df_long_jobs, df_short_jobs
+    return df_long_jobs, df_short_jobs, trace_name, basename
 
 def partition_job(job, lst_partitioned_jobs):
     ##dirty hack to deal with jobs that finish on the walltime
@@ -113,7 +127,7 @@ def partition_job(job, lst_partitioned_jobs):
         lst_partitioned_jobs.append(partition)
 
 
-def partition_workload(df_long_jobs, df_short_jobs):
+def partition_workload(df_long_jobs, df_short_jobs, trace_name, base_name, dct_workload_props):
     lst_partitioned_jobs=[]
 
     df_long_jobs.apply(lambda job: partition_job(job, lst_partitioned_jobs), axis=1)
@@ -137,10 +151,23 @@ def partition_workload(df_long_jobs, df_short_jobs):
     df_partitioned_workload=pd.concat([df_renamed_short_jobs, df_partitioned_jobs])
     df_partitioned_workload=df_partitioned_workload.sort_values(by='SUBMIT_TIME').reset_index(drop=True)
 
-    df_partitioned_workload.to_csv('../workloads/scripttest_partitioned_mustang_release_v0.2.0_66.swf', 
+    swf_outfile_name='../workloads/swf/partitioned/two_weeks/'+trace_name+'/'+base_name
+    df_partitioned_workload.to_csv(swf_outfile_name, 
                                    sep=' ', header=False, index=False)
 
     #call swftojson and save the json file
+    json_outfile_name='../workloads/json/partitioned/two_weeks/'+trace_name+'/'+re.sub('.swf', '', base_name)+'.json'
+    lst_call_command=['python',
+                      'swf_to_batsim_workload_compute_only.py',
+                      '-q',
+                      '-cs', JSON_COMP_SPEED,
+                      '-pf',
+                      str(dct_workload_props[trace_name].max_procs),
+                      swf_outfile_name,
+                      json_outfile_name]
+    #print(lst_call_command)
+    subprocess.call(lst_call_command)
+ 
 
 
 def main():
@@ -149,19 +176,22 @@ def main():
 
     Parses the input arguments then perform the workload partitioning.
     """
-    #parser = argparse.ArgumentParser(
-    #    description='Reads a folder of SWF files and creates to copies (one SWF and another json) with some jobs partitiones')
-    #parser.add_argument('input_swf_path', type=argparse.FileType('r'),
-    #                    help='The input SWF files folder')
+    parser = argparse.ArgumentParser(
+        description='Reads a folder of SWF files and creates to copies (one SWF and another json) with some jobs partitiones')
+    parser.add_argument('--input_swf_path', type=str,
+                        help='The input SWF files folder')
    
 
-    #args = parser.parse_args()
-    swf_input_path='../workloads/test_mustang_release_v0.2.0_66.swf'
-    swf_output_path='../workloads/partitioned/'
+    args = parser.parse_args()
+    swf_input_path=args.input_swf_path
+    #swf_input_path='../workloads/test_mustang_release_v0.2.0_66.swf'
+    #swf_output_path='../workloads/partitioned/'
     dct_workload_props = init()
-    if swf_input_path.endswith('.swf'):        
-        df_long_jobs, df_short_jobs = read_input_swf(swf_input_path, dct_workload_props)
-        partition_workload(df_long_jobs, df_short_jobs)
+    for file_str in os.listdir(swf_input_path):
+        if file_str.endswith('.swf'):
+            file_path=swf_input_path+'/'+file_str
+            df_long_jobs, df_short_jobs, trace_name, base_name = read_input_swf(file_path, dct_workload_props)
+            partition_workload(df_long_jobs, df_short_jobs, trace_name, base_name, dct_workload_props)
     
 if __name__ == "__main__":
     main()
